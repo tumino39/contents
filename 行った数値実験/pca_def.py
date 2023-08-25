77d9bda11629110172afebd3134484ab0614a51f
海野哲也 <umitetsu39@gmail.com>
2023-08-26 00:20:07 +0900

    数値実験1のコード

---
+++ 行った数値実験/pca_def.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+def conv_pca(X):
+    d = X.shape[0]
+    n = X.shape[1]
+    barX = np.dot(X.mean(axis = 1).reshape(d,1),np.ones(n).reshape(1,n))
+    X1 = X - barX
+    if n >d:
+        S = (1/(n-1)) * X1 @ X1.T
+        lam2,vec2 = np.linalg.eigh(S)
+        sort2  = lam2.argsort()[::-1]
+        lam2 = lam2[sort2]
+        vec_2t = (vec2.T[sort2]).T
+    else:
+        S = (1/(n-1)) * X1.T @ X1
+        lam2,vec2 = np.linalg.eigh(S)
+        sort2  = lam2.argsort()[::-1]
+        lam2 = lam2[sort2]
+        vec2 = (vec2.T[sort2]).T
+        vec_2t = np.zeros((d,n-1))
+        for i in range(n-1):
+            vec_2t[:,i] = np.dot((1/np.sqrt(lam2[i] * (n-1))) * X1,vec2[:,i])
+    return(lam2,vec_2t)
+
+def sph_pca(X):
+    d = X.shape[0]
+    n = X.shape[1]
+    cenX = np.dot(np.median(X,axis=1).reshape(d,1),np.ones(n).reshape(1,n))
+    X_s1 = X - cenX
+    X_s = np.zeros((d,n))
+    N = np.zeros(n)
+    for i in range(n):
+        X_s[:,i] =X_s1[:,i]/ np.linalg.norm(X_s1[:,i],ord = 1)
+        N[i] = np.linalg.norm(X_s1[:,i])
+    barX_s = np.dot(X_s.mean(axis = 1).reshape(d,1),np.ones(n).reshape(1,n))
+    X3 = X_s - barX_s
+    if n > d:
+        S_s = (1/(n-1)) * X3 @ X3.T
+        lam3,vec3 = np.linalg.eigh(S_s)
+        sort3  = lam3.argsort()[::-1]
+        lam3 = lam3[sort3]
+        vec_3t = (vec3.T[sort3]).T
+    else:
+        S_s = (1/(n-1)) * X3.T @ X3
+        lam3,vec3 = np.linalg.eigh(S_s)
+        sort3  = lam3.argsort()[::-1]
+        lam3 = lam3[sort3]
+        vec3 = (vec3.T[sort3]).T
+        vec_3t = np.zeros((d,n-1))
+        for i in range(n-1):
+            vec_3t[:,i] = np.dot((1/np.sqrt(lam3[i] * (n-1))) * X3,vec3[:,i])
+    return(lam3,vec_3t)
+
---
+++ 行った数値実験/simulation1.py
@@ -0,0 +1,52 @@
+import matplotlib
+import japanize_matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import imshow
+from numpy.random import randn
+from pca_def import conv_pca,sph_pca
+
+
+ite = 100
+box_conv = np.zeros((ite,6))
+box_sph = np.zeros((ite,6))
+alpha1 = 7/5
+alpha2 = 2/5
+data_n = 20#メインのデータ数
+out_n = 2#異常値の数
+
+for dim in range(6):
+    d  = 2**(dim + 5)
+    alpha1 = 7/5
+    alpha2 = 2/5
+
+    sig1 = np.zeros(d)
+    sig1[0:2] = np.array([d**alpha2,d**alpha1])
+    sig1 = np.diag(sig1)
+    sig2 = np.zeros((d,d))
+    for i in range(d):
+        for j in range(d):
+            sig2[i,j] = 1/(np.abs(i - j) + 1)
+    sigma1 = sig1 + sig2
+    for it in range(ite):
+        x1 = np.random.multivariate_normal(np.zeros(d),sigma1,data_n).T
+        x2 = np.random.multivariate_normal(np.ones(d),2*d * np.identity(d),out_n).T
+        X = np.concatenate([x1,x2],axis = 1)
+        d,n = np.shape(X)
+        lam1,vec1 = np.linalg.eigh(sigma1)
+        sort1  = lam1.argsort()[::-1]
+        lam1 = lam1[sort1]
+        vec1 = (vec1.T[sort1]).T
+
+        lam2,vec2  = conv_pca(X)
+        lam3,vec3 = sph_pca(X)
+
+        box_conv[it,dim] = np.abs(np.dot(vec1[:,0],vec2[:,0]))
+        box_sph[it,dim] = np.abs(np.dot(vec1[:,0],vec3[:,0]))
+        print('finish'+ str(dim) + '-' + str(it))
+
+plt.plot(np.arange(5,11),box_conv.mean(axis = 0),c = "b",marker = ".",label = "PCA")
+plt.plot(np.arange(5,11),box_sph.mean(axis = 0),c = "r",marker = ".",label = "SPCA")
+plt.xlabel("$log_2 d$")
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+plt.show()
